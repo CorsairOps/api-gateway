1221f4625259360b7402ee6b666c6719e4eb5407
Tyler Wade <vaxonreal@gmail.com>
2025-10-06 12:03:16 -0400

    remove unused authentication endpoints and related security annotations

--- src/main/java/com/corsairops/gateway/CorsairopsApiGatewayApplication.java
+++ src/main/java/com/corsairops/gateway/CorsairopsApiGatewayApplication.java
@@ -2,14 +2,8 @@ package com.corsairops.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -17,22 +11,6 @@ public class CorsairopsApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CorsairopsApiGatewayApplication.class, args);
-    }
-
-    @GetMapping("/token")
-    public Jwt getToken(@AuthenticationPrincipal Jwt jwt) {
-        return jwt;
-    }
-    
-    @GetMapping("/authentication")
-    public Authentication getAuthentication(Authentication authentication) {
-        return authentication;
-    }
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping("/admin-only")
-    public Map<String, String> adminOnly() {
-        return Map.of("message", "Hello, Admin!");
     }
 
 }
